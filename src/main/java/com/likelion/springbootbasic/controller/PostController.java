4a95bdffa87d1699ce291e139a56f8112b2fbdaa
Changbum97 <chb2005@naver.com>
2022-10-27 14:16:41 +0900

    post request body, dto 실습

--- src/main/java/com/likelion/springbootbasic/controller/PostController.java
+++ src/main/java/com/likelion/springbootbasic/controller/PostController.java
@@ -4,6 +4,7 @@ import com.likelion.springbootbasic.domain.dto.MemberDto;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1/post-api")
@@ -12,5 +13,17 @@ public class PostController {
     @PostMapping("/domain")
     public String postExample() {
         return "Hello Post API";
+    }
+
+    @PostMapping("/member")
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
+        postData.entrySet().forEach(map -> sb.append(map.getKey() + ":" + map.getValue()).append("\n"));
+        return sb.toString();
+    }
+
+    @PostMapping("/member-dto")
+    public String postMemberDto(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
     }
 }
