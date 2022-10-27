7405c93c89ded4dac34bcddfb2a802871d1a4c31
Changbum97 <chb2005@naver.com>
2022-10-27 14:41:40 +0900

    put

---
+++ src/main/java/com/likelion/springbootbasic/controller/PutController.java
@@ -0,0 +1,25 @@
+package com.likelion.springbootbasic.controller;
+
+import com.likelion.springbootbasic.domain.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+
+    @PutMapping("/domain")
+    public String putExample() {
+        return "Hello Put API";
+    }
+
+    @PutMapping("/member")
+    public ResponseEntity<MemberDto> putMember(@RequestBody MemberDto memberDto) {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(memberDto);
+    }
+}
