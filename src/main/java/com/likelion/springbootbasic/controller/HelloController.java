9b525eefa7ab02275c4c87a809d834d8e608b0d6
Changbum97 <chb2005@naver.com>
2022-10-27 11:29:48 +0900

    path variable, query parameter 실습

--- src/main/java/com/likelion/springbootbasic/controller/HelloController.java
+++ src/main/java/com/likelion/springbootbasic/controller/HelloController.java
@@ -1,9 +1,6 @@
 package com.likelion.springbootbasic.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
@@ -19,4 +16,15 @@ public class HelloController {
     public String getPathVariable(@PathVariable int v1, @PathVariable int v2) {
         return "Path Variable : " + (v1 + v2);
     }
+
+    @GetMapping("/query-param")
+    public String getQueryParam(@RequestParam String name, @RequestParam String email, @RequestParam String organization) {
+        return "Name : " + name + "\nEmail : " + email + "\nOranization : " + organization;
+    }
+
+    @GetMapping("/both-param/{v1}")
+    public String getBothParam(@PathVariable int v1, @RequestParam int v2) {
+        return "Path Variable : " + v1 + "\nQuery Param : " + v2;
+    }
+
 }
