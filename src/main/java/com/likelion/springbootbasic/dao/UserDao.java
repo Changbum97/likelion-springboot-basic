233a0f8ed2db12ca050cdd742e33978961a700ed
Changbum97 <chb2005@naver.com>
2022-10-28 13:39:58 +0900

    User Dao 생성

--- src/main/java/com/likelion/springbootbasic/dao/UserDao.java
+++ src/main/java/com/likelion/springbootbasic/dao/UserDao.java
@@ -1,2 +1,29 @@
-package com.likelion.springbootbasic.dao;public class UserDao {
+package com.likelion.springbootbasic.dao;
+
+import com.likelion.springbootbasic.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+public class UserDao {
+
+    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserDao(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void add(User user) {
+        this.jdbcTemplate.update("INSERT INTO users (id, name, password) VALUES (?, ?, ?)",
+                user.getId(), user.getName(), user.getPassword());
+    }
+
+    public int deleteAll() {
+        return this.jdbcTemplate.update("DELETE FROM users");
+    }
+
 }
