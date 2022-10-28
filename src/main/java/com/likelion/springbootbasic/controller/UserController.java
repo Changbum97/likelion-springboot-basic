f395bbd5c721127095908166883a5d6e46850018
Changbum97 <chb2005@naver.com>
2022-10-28 15:19:07 +0900

    User Controller URL 수정

--- src/main/java/com/likelion/springbootbasic/controller/UserController.java
+++ src/main/java/com/likelion/springbootbasic/controller/UserController.java
@@ -10,19 +10,19 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/user")
 public class UserController {
 
     @Autowired
     private UserDao userDao;
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello World";
     }
 
     // User을 Request Body로 받아 추가 후 User Return
-    @PostMapping("/user")
+    @PostMapping("")
     public User add(@RequestBody UserRequestDto userRequestDto) {
         User user = new User(userRequestDto.getId(), userRequestDto.getName(), userRequestDto.getPassword());
         userDao.add(user);
@@ -30,7 +30,7 @@ public class UserController {
     }
 
     // User 전체 삭제
-    @DeleteMapping("/user")
+    @DeleteMapping("")
     public ResponseEntity<Integer> deleteAll() {
         return ResponseEntity
                 .ok()
@@ -38,19 +38,19 @@ public class UserController {
     }
 
     // User 조회
-    @GetMapping("/user/{id}")
+    @GetMapping("/{id}")
     public User findById(@PathVariable String id) {
         return userDao.findById(id);
     }
 
     // User 전체 조회
-    @GetMapping("/user")
+    @GetMapping("")
     public List<User> findAll() {
         return userDao.findAll();
     }
 
     // 전체 User 수 조회
-    @GetMapping("/userCount")
+    @GetMapping("/count")
     public int getCount() {
         return userDao.getCount();
     }
