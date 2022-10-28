fc9efb734daaa0e500eed613d271b1dc7bcc1627
Changbum97 <chb2005@naver.com>
2022-10-28 14:41:18 +0900

    User Request Dto 수정

--- src/main/java/com/likelion/springbootbasic/domain/dto/UserRequestDto.java
+++ src/main/java/com/likelion/springbootbasic/domain/dto/UserRequestDto.java
@@ -7,4 +7,10 @@ public class UserRequestDto {
     private String id;
     private String name;
     private String password;
+
+    public UserRequestDto(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
 }
