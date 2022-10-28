39a132c0d95e551196abe1188eebc859c9807f23
Changbum97 <chb2005@naver.com>
2022-10-28 13:53:02 +0900

    User Request Dto 적용

---
+++ src/main/java/com/likelion/springbootbasic/domain/dto/UserRequestDto.java
@@ -0,0 +1,10 @@
+package com.likelion.springbootbasic.domain.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserRequestDto {
+    private String id;
+    private String name;
+    private String password;
+}
