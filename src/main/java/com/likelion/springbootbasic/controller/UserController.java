1f2b4368ac54a902e623c6d07eb77a0f4922d379
Changbum97 <chb2005@naver.com>
2022-10-28 14:27:23 +0900

    UserDao에 있던 다른 기능들 구현

--- src/main/java/com/likelion/springbootbasic/controller/UserController.java
+++ src/main/java/com/likelion/springbootbasic/controller/UserController.java
@@ -6,6 +6,8 @@ import com.likelion.springbootbasic.domain.dto.UserRequestDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
@@ -21,16 +23,37 @@ public class UserController {
         return "Hello World";
     }
 
+    // User을 Request Body로 받아 추가 후 User Return
     @PostMapping("/user")
-    public void add(@RequestBody UserRequestDto userRequestDto) {
+    public User add(@RequestBody UserRequestDto userRequestDto) {
         User user = new User(userRequestDto.getId(), userRequestDto.getName(), userRequestDto.getPassword());
         userDao.add(user);
+        return userDao.findById(user.getId());
     }
 
+    // User 전체 삭제
     @DeleteMapping("/user")
     public ResponseEntity<Integer> deleteAll() {
         return ResponseEntity
                 .ok()
                 .body(userDao.deleteAll());
+    }
+
+    // User 조회
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable String id) {
+        return userDao.findById(id);
+    }
+
+    // User 전체 조회
+    @GetMapping("/user")
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+
+    // 전체 User 수 조회
+    @GetMapping("/user/count")
+    public int getCount() {
+        return userDao.getCount();
     }
 }
