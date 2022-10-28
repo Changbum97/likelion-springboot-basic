6354011b765db5ebc284c3d21d4aa8c637572b68
Changbum97 <chb2005@naver.com>
2022-10-28 13:45:15 +0900

    User Controller

--- src/main/java/com/likelion/springbootbasic/controller/UserController.java
+++ src/main/java/com/likelion/springbootbasic/controller/UserController.java
@@ -1,2 +1,37 @@
-package com.likelion.springbootbasic.controller;public class UserController {
+package com.likelion.springbootbasic.controller;
+
+import com.likelion.springbootbasic.dao.UserDao;
+import com.likelion.springbootbasic.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+
+    private final UserDao userDao;
+
+    public UserController(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @PostMapping("/user")
+    public void add() {
+        User user = new User("1", "Changbum", "1234");
+        userDao.add(user);
+    }
+
+    public ResponseEntity<Integer> deleteAll() {
+        return ResponseEntity
+                .ok()
+                .body(userDao.deleteAll());
+    }
 }
