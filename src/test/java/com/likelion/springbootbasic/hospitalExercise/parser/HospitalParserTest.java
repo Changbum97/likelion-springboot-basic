9406ab6ed831e3a9026a9b5429e821d255393972
Changbum97 <chb2005@naver.com>
2022-10-31 15:03:40 +0900

    Hospital Parser Test

--- src/test/java/com/likelion/springbootbasic/hospitalExercise/parser/HospitalParserTest.java
+++ src/test/java/com/likelion/springbootbasic/hospitalExercise/parser/HospitalParserTest.java
@@ -1,10 +1,15 @@
 package com.likelion.springbootbasic.hospitalExercise.parser;
 
 import com.likelion.springbootbasic.hospitalExercise.domain.Hospital;
+import com.likelion.springbootbasic.hospitalExercise.domain.dao.HospitalDao;
+import com.zaxxer.hikari.util.DriverDataSource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
