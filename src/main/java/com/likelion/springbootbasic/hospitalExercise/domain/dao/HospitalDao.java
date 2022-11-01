7f598acf6a09385b3f3be20422c408ecd5ea51fe
Changbum97 <chb2005@naver.com>
2022-11-01 11:19:09 +0900

    DB Insert 테스트 성공

--- src/main/java/com/likelion/springbootbasic/hospitalExercise/domain/dao/HospitalDao.java
+++ src/main/java/com/likelion/springbootbasic/hospitalExercise/domain/dao/HospitalDao.java
@@ -1,2 +1,31 @@
-package com.likelion.springbootbasic.hospitalExercise.domain.dao;public class HospitalDao {
+package com.likelion.springbootbasic.hospitalExercise.domain.dao;
+
+import com.likelion.springbootbasic.hospitalExercise.domain.Hospital;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class HospitalDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public HospitalDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void add(Hospital hospital) {
+        jdbcTemplate.update("INSERT INTO nation_wide_hospitals(id, open_service_name, open_local_government_code," +
+                "management_number, license_date, business_status, business_status_code, phone," +
+                "full_address, road_name_address, hospital_name, business_type_name," +
+                "healthcare_provider_count, patient_room_count, total_number_of_beds, total_area_size)" +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                hospital.getId(), hospital.getOpenServiceName(), hospital.getOpenLocalGovernmentCode(),
+                hospital.getManagementNumber(), hospital.getLicenseDate(), hospital.getBusinessStatus(),
+                hospital.getBusinessStatusCode(), hospital.getPhone(), hospital.getFullAddress(),
+                hospital.getRoadNameAddress(), hospital.getHospitalName(), hospital.getBusinessTypeName(),
+                hospital.getHealthcareProviderCount(), hospital.getPatientRoomCount(),
+                hospital.getTotalNumberOfBeds(), hospital.getTotalAreaSize());
+    }
 }
--- src/test/java/com/likelion/springbootbasic/hospitalExercise/domain/dao/HospitalDaoTest.java
+++ src/test/java/com/likelion/springbootbasic/hospitalExercise/domain/dao/HospitalDaoTest.java
@@ -1,4 +1,35 @@
+package com.likelion.springbootbasic.hospitalExercise.domain.dao;
+
+import com.likelion.springbootbasic.hospitalExercise.domain.Hospital;
+import com.likelion.springbootbasic.hospitalExercise.parser.ReadLineContext;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.io.IOException;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class HospitalDaoTest {
-  
+
+    @Autowired
+    ReadLineContext<Hospital> hospitalReadLineContext;
+
+    @Autowired
+    HospitalDao hospitalDao;
+
+
+    @Test
+    @DisplayName("CSV 첫 줄을 DB에 삽입하는 Test")
+    void addTest() throws IOException {
+        List<Hospital> hospitalList = hospitalReadLineContext.readByLine("./hospital_data.csv");
+        Hospital hospital1 = hospitalList.get(0);
+        System.out.println(hospital1.getHospitalName());
+
+        hospitalDao.add(hospital1);
+    }
 }
