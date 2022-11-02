e1ec2259bdc955a5ee4edc139847c86b91a2472a
Changbum97 <chb2005@naver.com>
2022-11-02 11:40:03 +0900

    Hospital toString Overriding

--- src/main/java/com/likelion/springbootbasic/hospitalExercise/domain/Hospital.java
+++ src/main/java/com/likelion/springbootbasic/hospitalExercise/domain/Hospital.java
@@ -24,9 +24,32 @@ public class Hospital {
     private String roadNameAddress;         // 도로명 전체 주소
     private String hospitalName;            // 사업장 이름
     private String businessTypeName;        // 업태 구분명
-    private int healthcareProviderCount;    // 의료인
+    private int healthcareProviderCount;    // 의료인 수
     private int patientRoomCount;           // 입원실 수
     private int totalNumberOfBeds;          // 병상 수
     private float totalAreaSize;            // 총 면적
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("id : ").append(id).append("\n");
+        sb.append("개방 서비스명 : ").append(openServiceName).append("\n");
+        sb.append("개방 자치 단체 코드 : ").append(openLocalGovernmentCode).append("\n");
+        sb.append("관리 번호 : ").append(managementNumber).append("\n");
+        sb.append("인/허가 일자 : ").append(licenseDate).append("\n");
+        sb.append("영업 상태 구분 : ").append(businessStatus).append("\n");
+        sb.append("상세 영업 상태 코드 : ").append(businessStatusCode).append("\n");
+        sb.append("전화 번호 : ").append(phone).append("\n");
+        sb.append("소재지 전체 주소 : ").append(fullAddress).append("\n");
+        sb.append("도로명 전체 주소 : ").append(roadNameAddress).append("\n");
+        sb.append("병원명 : ").append(hospitalName).append("\n");
+        sb.append("업태 구분명 : ").append(businessTypeName).append("\n");
+        sb.append("의료인 수 : ").append(healthcareProviderCount).append("\n");
+        sb.append("입원실 수 : ").append(patientRoomCount).append("\n");
+        sb.append("병상 수 : ").append(totalNumberOfBeds).append("\n");
+        sb.append("총 면적 : ").append(totalAreaSize).append("\n");
+
+        return sb.toString();
+    }
 }
