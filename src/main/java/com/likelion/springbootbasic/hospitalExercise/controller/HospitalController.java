f8bd52e244e32874023a03b001782cf38d7bde69
Changbum97 <chb2005@naver.com>
2022-11-02 11:23:23 +0900

    REST API로 병원 데이터 전체 삽입, 삭제 구현

--- src/main/java/com/likelion/springbootbasic/hospitalExercise/controller/HospitalController.java
+++ src/main/java/com/likelion/springbootbasic/hospitalExercise/controller/HospitalController.java
@@ -1,15 +1,53 @@
 package com.likelion.springbootbasic.hospitalExercise.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.likelion.springbootbasic.hospitalExercise.domain.Hospital;
+import com.likelion.springbootbasic.hospitalExercise.domain.dao.HospitalDao;
+import com.likelion.springbootbasic.hospitalExercise.parser.ReadLineContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/hospital")
 public class HospitalController {
 
+    @Autowired
+    HospitalDao hospitalDao;
+
+    @Autowired
+    ReadLineContext<Hospital> hospitalReadLineContext;
+
     @GetMapping({"", "/"})
     public String hello() {
         return "Hello";
+    }
+
+    @PostMapping("/all")
+    public String insertAll() {
+        try {
+            List<Hospital> hospitalList = hospitalReadLineContext.readByLine("./hospital_data.csv");
+            for(Hospital hospital : hospitalList) {
+                hospitalDao.add(hospital);
+            }
+
+            return hospitalList.size() + "개 병원 데이터 삽입 성공";
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "전체 삽입 실패";
+        }
+    }
+
+    @DeleteMapping("/all")
+    public String deleteAll() {
+        try {
+            hospitalDao.deleteAll();
+            return "전체 삭제 성공";
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "전체 삭제 실패";
+        }
     }
 }
